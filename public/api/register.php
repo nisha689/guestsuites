446e76f432f4a74b434c85080d82b5f0628ad896
nisha689 <63635166+nisha689@users.noreply.github.com>
2020-04-16 17:59:55 +0530

    register api

--- public/api/register.php
+++ public/api/register.php
@@ -9,23 +9,18 @@ $response_data = array();
 $token = $_POST['token'];
 $check_token = verify_token($token);
 
-if($check_token[0]=='1'){
+if($check_token[0]=='1') {
 
-	$first_name = mysqlRealescapestring($conn,$_POST['first_name']);
-	$last_name = mysqlRealescapestring($conn,$_POST['last_name']);
-	$email = mysqlRealescapestring($conn,$_POST['email']);
-	$social_type = !empty($_POST['social_type']) ? mysqlRealescapestring($conn,$_POST['social_type']) : NULL;
-	$password = NULL;
-	$confirm_password = NULL;
-	
-	if($social_type > 0){
-		$char_count = 10;
-	}else{
-		$password = mysqlRealescapestring($conn,$_POST['password']);
-		$confirm_password = mysqlRealescapestring($conn,$_POST['confirm_password']);
-		$char_count = strlen($password);
-	}
-	$role_id = mysqlRealescapestring($conn,$_POST['role_id']);
+	$first_name			=	mysqlRealescapestring($conn,$_POST['first_name']);
+	$last_name 			=	mysqlRealescapestring($conn,$_POST['last_name']);
+	$email 				=	mysqlRealescapestring($conn,$_POST['email']);	
+	$phone 				=	mysqlRealescapestring($conn,$_POST['phone']);	
+	$role_id 			= 	mysqlRealescapestring($conn,$_POST['role_id']);
+	$business_type		= 	mysqlRealescapestring($conn,$_POST['business_type']);
+	$password 			= 	mysqlRealescapestring($conn,$_POST['password']);
+	$confirm_password	= 	mysqlRealescapestring($conn,$_POST['confirm_password']);
+	$char_count 		= 	!empty($password) ? strlen($password) : 0;
+		
 	
 	// check email is exists or not
 	$check_qry_var = "SELECT email FROM ".DB_PREFIX."users WHERE email='".$email."'";
@@ -37,7 +32,7 @@ if($check_token[0]=='1'){
 	$check_role_qry = mysqlQuery($conn,$check_role_qry_var);
 	$check_role = myasqlNumRow($check_role_qry);
 	
-	if($first_name == '' || $last_name == '' || $email == '' || $role_id==''){
+	if($first_name == '' || $last_name == '' || $email == '' || $role_id=='' || $business_type=='' || $phone = ''){
 		$response_data['response_code'] = '0';
 		$response_data['response_error'] = BLANK_VALIDATION_MSG;
 		$response_data['response_data'] = array();
@@ -71,17 +66,19 @@ if($check_token[0]=='1'){
 		$response_data['response_success'] = '';
 		$response = $response_data;
 	
-	}elseif($char_count < 7){
+	}
+	elseif($password == '' ){
+		$response_data['response_code'] = '0';
+		$response_data['response_error'] = "Please enter password";
+		$response_data['response_data'] = array();
+		$response_data['response_success'] = '';
+	} 
+	elseif($char_count < 7){
 		$response_data['response_code'] = '0';
 		$response_data['response_error'] = "Please enter at least 7 characters for your password!";
 		$response_data['response_data'] = array();
 		$response_data['response_success'] = '';
 	
-	}elseif($password == '' && !($social_type > 0)){
-		$response_data['response_code'] = '0';
-		$response_data['response_error'] = "Please enter password";
-		$response_data['response_data'] = array();
-		$response_data['response_success'] = '';
 	}
 	elseif($password!=$confirm_password){
 		$response_data['response_code'] = '0';
@@ -91,26 +88,25 @@ if($check_token[0]=='1'){
 	
 	}else{
 		
-		$user_data['first_name'] = ucfirst($first_name);
-		$user_data['last_name'] = ucfirst($last_name);
-		$user_data['email'] = $email;
-		if(! $social_type > 0){
-			$user_data['password'] = PasswordGenerator($password);
-		}
-		$user_data['email_verified_at'] = get_current_date_time();
-		$user_data['created_at'] = get_current_date_time();
-		$user_data['role_id'] = $role_id;
-		$user_data['created_type'] = 'App';
-		$user_data['ip_address'] = $_SERVER['REMOTE_ADDR'];
-		$user_data['status'] = '1';
-		$user_data['social_type'] = $social_type;
+		$user_data['first_name'] 			= 	ucfirst($first_name);
+		$user_data['last_name'] 			= 	ucfirst($last_name);
+		$user_data['email'] 				= 	$email;
+		$user_data['phone'] 				= 	$phone;
+		$user_data['password'] 				= 	PasswordGenerator($password);
+		$user_data['email_verified_at'] 	=	get_current_date_time();
+		$user_data['created_at'] 			=	get_current_date_time();
+		$user_data['role_id'] 				=	$role_id;
+		$user_data['created_type'] 			=	'App';
+		$user_data['ip_address'] 			=	$_SERVER['REMOTE_ADDR'];
+		$user_data['status'] 				=	'1';		
+		$user_data['business_service_id'] 	=	$business_type;		
 		
 		$last_id = ins_rec(DB_PREFIX."users",$user_data);
 				
 		$response_data['response_code'] = '1';
 		$response_data['response_error'] = "";
 		$response_data['response_data']['user_id'] = $last_id;
-		$response_data['response_success'] = 'You have signed up successfully.';
+		$response_data['response_success'] = 'You have signed up successfully.';		
 	}
 	
 }else{
