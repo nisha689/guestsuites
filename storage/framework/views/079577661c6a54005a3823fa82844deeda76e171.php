8ebe3ebf8810536a42f19f07407d0eba80992132
Ankur DalsaniYa <dalsaniaankur@gmail.com>
2020-04-24 10:38:15 +0530

    Remove Unnecessary file

--- storage/framework/views/079577661c6a54005a3823fa82844deeda76e171.php
+++
@@ -1,6 +0,0 @@
-<?php if(session('success')): ?>
-<div class="notification success successmsg">
-  <span><?php echo e(session('success')); ?></span>
-  <button class="close-ntf"><i class="fas fa-times"></i></button>
-</div>
-<?php endif; ?>
--- storage/framework/views/a60d7eba2443b68ad4a60605b9c21bfe7d1c7716.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection('title', __('Method Not Allowed')); ?>
-<?php $__env->startSection('code', '405'); ?>
-<?php $__env->startSection('message', __('Sorry, Method not allowed.')); ?>
-<?php echo $__env->make('errors.layout', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
